65a43f84e7ba9defe2b1f510ebc26ce7b0a43bac
Mustafa Kutlimetov <kutlimetov98@bk.ru>
2022-11-25 16:07:54 +0300

    added counter for hero section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function(){
 
+  /* ИНИЦИАЛИЗАЦИЯ БИБЛИОТЕК */
+    /* typed */
+  const typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+  });
 });
