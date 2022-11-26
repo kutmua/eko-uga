62069d37049d4eed52cd6c4207bfe960d02902c1
Mustafa Kutlimetov <kutlimetov98@bk.ru>
2022-11-26 15:32:35 +0300

    added reviews section

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
   /* ИНИЦИАЛИЗАЦИЯ БИБЛИОТЕК */
-    /* typed */
+  /* typed */
   const typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
     typeSpeed: 150,
     backSpeed: 100,
     loop: true,
     loopCount: Infinity,
+  });
+
+  /* reviews-swiper */
+  const swiper = new Swiper('.reviews-swiper', {
+    direction: 'horizontal',
+
+    pagination: {
+      el: '.reviews-swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.reviews-swiper-btn-next',
+      prevEl: '.reviews-swiper-btn-prev',
+    },
+
   });
 });
